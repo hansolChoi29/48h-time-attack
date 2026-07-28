03b728ee3b8f9723f7762abbd31019637d76249b
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-07-28 19:52:10 +0900

    chore: 서비스 패키지 구조 정리

--- notification-service/src/main/java/com/spring/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/spring/notificationservice/NotificationServiceApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableFeignClients
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
 }
