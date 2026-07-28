43d561fe2900321bf2c81b8739b3bc04d38a1fc9
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-07-28 17:19:05 +0900

    init: project setup

---
+++ delivery-service/src/test/java/com/spring/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/main/java/com/spring/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.spring.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableFeignClients
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
